220087a0d3eb09b746b5ece245827f66a5401498
akimasa-l <liumingzheng8@outlook.com>
2021-04-02 15:23:56 +0900

    add download scripts

---
+++ download_pdf.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+
+def download_pdf(url):
+    response = requests.get(url)
+    print(response.status_code)
+    print(response.headers['Content-Disposition'])  # TODO:ここからファイル名を取り出す方法
+    with open("./a.pdf", mode="wb") as f:
+        f.write(response.content)
+
+if __name__ == '__main__':
+    url = "https://drive.google.com/uc?id=1to7aAGQ7hmZseactjOiXwDTHl2Dgfhze&export=download"
+    download_pdf(url)
+
+
+# 例："Content-Disposition": "attachment;filename=\"__ - AtCoder Beginner Contest 194 - ___.pdf\";filename*=UTF-8''%E8%A7%A3%E8%AA%AC%20-%20AtCoder%20Beginner%20Contest%20194%20-%20%E5%8A%89%E6%98%8E%E6%AD%A3.pdf",
