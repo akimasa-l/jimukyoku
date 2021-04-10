884eb390d645dc7acbd4d8a838b5e2d84057c2c8
akimasa-l <liumingzheng8@outlook.com>
2021-04-10 22:11:24 +0900

    Update add_DB_async.py

--- add_DB_async.py
+++ add_DB_async.py
@@ -1,11 +1,20 @@
 import asyncio
+import json
+
 import aiohttp
 
-DBurl="google.com"
+DBurl = "google.com"
 
-async def add_DB(id:str):
-    params={"id":id}
+
+async def add_DB(id: str):
+    params = {"id": id}
     async with aiohttp.ClientSession() as session:
-        async with session.get(DBurl,params=params) as r:
+        async with session.get(DBurl, params=params) as r:
             pass
-    
+
+
+async def get_DB():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(DBurl) as r:
+            text = r.text()
+    return json.loads(text)
